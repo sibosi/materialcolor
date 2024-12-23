5d713f3ec21d9c14320710b1d0dd8fa73fbd7ecb
Nimród Simon <snimrod28@gmail.com>
2024-12-24 00:00:25 +0100

    add service worker and manifest for Material Color Generator

---
+++ public/serviceWorker.js
@@ -0,0 +1,22 @@
+// public/service-worker.js
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("static-cache-v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/favico.ico",
+        "/android144.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
